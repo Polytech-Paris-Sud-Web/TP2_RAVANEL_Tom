5cf0b881174fc6d72b47ffa51d4f74dbf63e34e9
Tom <t.ravanel@orange.fr>
2022-05-19 13:30:55 +0200

    Cache correctif et chargement du cache au lancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ArticleSource } from './web-article/services/article.source';
+import { AuthorSource } from './web-author/services/author.source';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-app'; 
+  constructor(private articleSource:ArticleSource, private authorSource:AuthorSource){
+    const promise = new Promise((resolve, reject) => {
+      articleSource.getArticles().subscribe(articles => {
+        articles.forEach(article => {
+          articleSource.getArticle(article.id);          
+        });
+      });
+      authorSource.getAuthors();
+    });
+  }
 }
