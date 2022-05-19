eea58dd52320279c5c8f5c96cdad66dc7922cdc9
Tom <t.ravanel@orange.fr>
2022-05-19 13:46:30 +0200

    cache cec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ArticleSource } from './web-article/services/article.source';
-import { AuthorSource } from './web-author/services/author.source';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +7,4 @@ import { AuthorSource } from './web-author/services/author.source';
 })
 export class AppComponent {
   title = 'simple-app'; 
-  constructor(private articleSource:ArticleSource, private authorSource:AuthorSource){
-    const promise = new Promise((resolve, reject) => {
-      articleSource.getArticles().subscribe(articles => {
-        articles.forEach(article => {
-          articleSource.getArticle(article.id);          
-        });
-      });
-      authorSource.getAuthors();
-    });
-  }
 }
